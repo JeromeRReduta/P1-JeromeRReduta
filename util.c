72ef381eb3e049812e9afa6610f497daa0d0f546
JeromeRREduta <jrreduta@yahoo.com>
2021-02-22 20:18:02 -0800

    Deleted lineread.c/.h, moved code to util.c/.h

--- util.c
+++ util.c
@@ -43,3 +43,26 @@ int open_path(char *base, char *extension)
     return path_fd;
 }
 
+ssize_t lineread(int fd, char *buf, size_t sz)
+{
+
+    for (size_t i = 0; i < sz; i++) {
+        char c;
+        ssize_t bytesRead = read(fd, &c, 1);
+
+        // Case: EOF or error - return 0 (EOF) or -1 (error)
+        if (bytesRead <= 0) {
+            return bytesRead;
+        }
+        // Case: read is successful
+        buf[i] = c;
+       
+        if (c == '\n') {
+            return i+1;
+        }
+    }
+
+    // Case: All sz bytes have been read
+    return sz;
+}
+
--- util.h
+++ util.h
@@ -7,4 +7,24 @@ void draw_percbar(char *buf, double frac);
 void uid_to_uname(char *name_buf, uid_t uid);
 int open_path(char *base, char *extension);
 
+/**
+ * Reads a line from a file descriptor and copies its contents into a buffer.
+ * 
+ * Note: Only including in util because I can't figure out how to import this func from lineread.c and lineread.h
+ * TODO: Ask how to import this and ask about this error that shows up in my terminal:
+ * 
+ * [jrreduta@KevinM P1-JeromeRReduta]$ make
+ * cc -g -Wall -fPIC -DLOGGER=1 -lm -lncurses  inspector.o procfs.o display.o util.o -o inspector
+ * /usr/bin/ld: procfs.o: in function `pfs_hostname':
+ * /home/jrreduta/P1-JeromeRReduta/procfs.c:10: undefined reference to `lineread'
+
+ *
+ * @param fd File descriptor to read
+ * @param buf Destination buffer to copy line contents
+ * @param sz Maximum number of bytes read from the file descriptor
+ *
+ * @return number of bytes read, 0 if EOF is reached, -1 on error
+ */
+ssize_t lineread(int fd, char *buf, size_t sz);
+
 #endif
