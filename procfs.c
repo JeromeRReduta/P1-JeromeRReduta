47d9cfe45a58cb2df3644a878a1a6d08a2eacc3d
JeromeRREduta <jrreduta@yahoo.com>
2021-02-23 23:22:42 -0800

    Fixed wrong implementation of array

--- procfs.c
+++ procfs.c
@@ -235,7 +235,7 @@ double pfs_cpu_usage(char *proc_dir, struct cpu_stats *prev, struct cpu_stats *c
 
 int init_cpu_stats(char *proc_dir, struct cpu_stats *stats)
 {
-    char[256] buf;
+    char buf[256];
     char* buf_ptr;
 
     if (pfs_get_aspect(proc_dir, buf_ptr, 256, "stat") == -1) {
