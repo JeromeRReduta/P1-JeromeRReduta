62bd69e4d9d526bebbef2eb4b0951a15a8e6cf78
JeromeRREduta <jrreduta@yahoo.com>
2021-02-23 23:28:14 -0800

    Deleted unused code

--- procfs.c
+++ procfs.c
@@ -251,7 +251,6 @@ int init_cpu_stats(char *proc_dir, struct cpu_stats *stats)
     LOG("SEP_PTR VAL:\t%s\n", sep_ptr);
 
     // Because of earlier calloc, don't have to init values of stats
-    for (int i = 0; i < )
 
     return -1;
 
