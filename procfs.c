d7109aaab0febf8e79772a78c9d05032dc9f7613
JeromeRREduta <jrreduta@yahoo.com>
2021-02-23 16:40:39 -0800

    Created buffer string and allocated it

--- procfs.c
+++ procfs.c
@@ -90,7 +90,7 @@ double pfs_uptime(char *proc_dir)
     LOG("UPTIME STRING IS:\t%s\n", uptime_string);
 
     // length = 100 b/c we only need to store the other unused #
-    char buffer[100];
+    char* buffer = "This is just a buffer string for the other number";
 
     return strtod(uptime_string, &buffer);
 }
