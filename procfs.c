9e020e47d2e2f02a9b61bf4ea7428f91b04a0b6f
JeromeRReduta <jrreduta@yahoo.com>
2021-02-24 13:04:05 -0800

    fixed typo

--- procfs.c
+++ procfs.c
@@ -323,7 +323,7 @@ struct mem_stats pfs_mem_usage(char *proc_dir)
 
 
     char* mem_total_ptr = strsep(&mem_total_head, "\t");
-    char* mem_avali_ptr = "NOTHING YET";
+    char* mem_avail_ptr = "NOTHING YET";
 /*    char* mem_avail_ptr = strsep(&mem_avail_head, "\t");
 */
 
