c24c9d303646026283f95d2153e519ea49581f3a
JeromeRREduta <jrreduta@yahoo.com>
2021-02-24 00:40:53 -0800

    Made better logs hopefully

--- procfs.c
+++ procfs.c
@@ -235,7 +235,9 @@ double pfs_cpu_usage(char *proc_dir, struct cpu_stats *prev, struct cpu_stats *c
         return 0.0;
     }
 
-    LOG(" (curr->idle - prev->idle) / (curr->total - prev->total):\n(%ld - %ld)/(%ld - %ld)\t=\t%ld/%ld\n", curr->idle, prev->idle, curr->total, prev->total, (curr->idle - prev-> idle), (curr->total - prev-> total));
+    //LOG(" (curr->idle - prev->idle) / (curr->total - prev->total):\n(%ld - %ld)/(%ld - %ld)\t=\t%ld/%ld\n", curr->idle, prev->idle, curr->total, prev->total, (curr->idle - prev-> idle), (curr->total - prev-> total));
+    LOG("prev->idle:\t%ld\n, prev->total:\t%ld\n, current->idle:\t%ld\n, current->total:\t%ld\n",
+        prev->idle, prev->total, current->idle, current->total);
     double used = (double) ((curr->idle - prev->idle) / (curr->total - prev->total));
     LOG("USED:\t%f\n", used);
     
