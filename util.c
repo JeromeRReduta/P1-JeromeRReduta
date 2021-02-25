0be91f6103550c483f3dedc08c56b64363cbf96a
JeromeRReduta <jrreduta@yahoo.com>
2021-02-24 22:46:27 -0800

    Accidentally deleted open_path and lineread

--- util.c
+++ util.c
@@ -141,9 +141,62 @@ void uid_to_uname(char *name_buf, uid_t uid)
 
 
 
-close(passwd_fd);
+    close(passwd_fd);
 
 }
+
+// Lovingly ripped out of lab code - Note: HAVE to close @ end of func
+int open_path(char *base, char *extension)
+{
+    // Case: Invalid base or extension
+    if (base == NULL || extension == NULL) {
+        return -1;
+    }
+
+    // Add 2 for null terminators
+    size_t path_size = (strlen(base) + strlen(extension) + 2) * sizeof(char);
+    char* path = malloc(path_size);
+    // Apparently this will concat base and extension, and make it the value of path
+    snprintf(path, path_size, "%s/%s", base, extension);
+
+    // Case: Invalid full path
+    if (path == NULL) {
+        return -1;
+    }
+
+    LOG("Opening path: %s\n", path);
+    int path_fd = open(path, O_RDONLY);
+
+    // Once we have our fd, can free path
+    free(path);
+
+    return path_fd;
+}
+
+ssize_t lineread(int fd, char *buf, size_t sz)
+{
+
+    for (size_t i = 0; i < sz; i++) {
+        char c;
+        ssize_t bytesRead = read(fd, &c, 1);
+
+        // Case: EOF or error - return 0 (EOF) or -1 (error)
+        if (bytesRead <= 0) {
+            return bytesRead;
+        }
+        // Case: read is successful
+        buf[i] = c;
+       
+        if (c == '\n') {
+            buf[i] = '\0';
+            return i + 1;
+        }
+    }
+
+    // Case: All sz bytes have been read
+    return sz;
+}
+
 
 int pfs_get_aspect(char *proc_dir, char *buf, size_t buf_sz, char *extension)
 {
