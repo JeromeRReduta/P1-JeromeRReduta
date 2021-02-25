f0f72a6c924c021a69bf85653e9af56b70b0592c
JeromeRReduta <jrreduta@yahoo.com>
2021-02-24 20:40:00 -0800

    forgot to check if state_search != null

--- procfs.c
+++ procfs.c
@@ -497,13 +497,16 @@ void update_task_stats(int status_fd, struct task_stats *tstats)
     while ( (read_sz = lineread(status_fd, line, 256)) > 0) {
         char* state_search = strstr(line, "State:") + '\0';
 
-        LOG("FOUND STATE_SEARCH:\t%s\n", state_search);
 
-        char* state = strsep(&state_search, "State:  ");
+        if (state_search != NULL) {
+            LOG("FOUND STATE_SEARCH:\t%s\n", state_search);
 
-        LOG("STATE = %s\n", state);
+            char* state = strsep(&state_search, "State:  ");
 
-        state[1] = '\0';
+            LOG("STATE = %s\n", state);
+
+            state[1] = '\0';
+        }
 
 
 
