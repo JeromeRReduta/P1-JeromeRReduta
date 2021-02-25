ac5d9e01b4f6a9e77b3286505fdc44823f18af10
JeromeRReduta <jrreduta@yahoo.com>
2021-02-25 00:09:11 -0800

    atoui() doesn't exist, using atoi() instead

--- procfs.c
+++ procfs.c
@@ -587,9 +587,9 @@ pid_t get_task_pid(pid_t prev, char *line)
         char* pid_str = strsep(&pid_search, "Pid:") + 5;
         LOG("MATCH FOUND:\t%s\n", pid_str);
 
-        LOG("ATOUI(PID_STR) = %i", atoui(pid_str));
+        LOG("ATOUI(PID_STR) = %i", atoi(pid_str));
 
-        return atoui(pid_str);
+        return atoi(pid_str);
 
 
     }
