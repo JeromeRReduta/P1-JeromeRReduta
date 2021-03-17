cdcc01e00e8d502718317a7d335c4c145e27968b
JRReduta <JRReduta@yahoo.com>
2021-03-17 05:55:56 +0000

    Added template for code consistency

---
+++ template.c
@@ -0,0 +1,30 @@
+/**
+ * @file
+ *
+ * REPLACE_THIS_WITH_INFO
+ */
+
+#include <stdlib.h>
+#include "logger.h"
+
+/**
+ * Struct holding private data
+ */
+/* Private struct goes here */
+
+
+/**
+ * Ptrs to public and private data
+ */
+
+/* Public_Struct* prefix_globals */
+/* static Private_Struct* prefix_locals */
+
+/**
+ * Define constants below
+ */
+
+
+
+
+
---
+++ template.h
@@ -0,0 +1,21 @@
+/**
+ * @file
+ *
+ * REPLACE_THIS_WITH_INFO
+ */
+
+#ifndef _REPLACE_WITH_NAME_H
+#define _REPLACE_WITH_NAME_H
+
+
+/**
+ * Struct holding public data
+ */
+
+// Add public struct here
+// Add extern public struct* here
+// 
+// 
+
+
+#endif
